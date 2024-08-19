8ec5dfbb89085e6d31982f0a8a2c5e67ccec722c
Tamuz Huji <tamuz.gitler@mail.huji.ac.il>
2024-08-19 16:11:26 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -38,63 +38,59 @@ from jsCodeExamples import TITLES,TEMPLATES, SOLUTIONS, DESCRIPTIONS, TASKS
 ######################## code ########################
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'database/code_blocks_table.db')}"db.init_app(app)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
-cd_rooms = {}
+# app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'database/code_blocks_table.db')}"db.init_app(app)
+# CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 socketio_handler = SocketIOHandler(app)
 
 ######################## server requests ########################
 # not in used
 @app.route('/')
 def hello():
-    print("hii")
     return "Hello, World!"
 
-@app.route('/get_buttons_names/')
-def handle_get_buttons():
-    print("user entered lobby")
-    query = CodeBlock.query.with_entities(CodeBlock.code_block_id, CodeBlock.title).order_by(CodeBlock.code_block_id)
-    blocks = query.all() # fetches all the rows that match your query
-    data = [{"code_block_id":cb.code_block_id, "title": cb.title} for cb in blocks]
-    return jsonify(data)
+# @app.route('/get_buttons_names/')
+# def handle_get_buttons():
+#     print("user entered lobby")
+#     query = CodeBlock.query.with_entities(CodeBlock.code_block_id, CodeBlock.title).order_by(CodeBlock.code_block_id)
+#     blocks = query.all() # fetches all the rows that match your query
+#     data = [{"code_block_id":cb.code_block_id, "title": cb.title} for cb in blocks]
+#     return jsonify(data)
 
 
-@app.route('/codeblocks/<int:code_block_id>')  # captures the ID from the URL
-def get_code_block_data(code_block_id):
-    """
-    returns inital data
-    """
-    print("Getting offline code block data")
+# @app.route('/codeblocks/<int:code_block_id>')  # captures the ID from the URL
+# def get_code_block_data(code_block_id):
+#     """
+#     returns inital data
+#     """
+#     print("Getting offline code block data")
 
-    code_block = CodeBlock.query.get(code_block_id)
-    if code_block:
-        return jsonify({
-            'title': code_block.title,
-            'description': code_block.descriptions,
-            'task': code_block.task 
-        })
-    else:
-        return jsonify({"error": "Code block not found"}), 404
+#     code_block = CodeBlock.query.get(code_block_id)
+#     if code_block:
+#         return jsonify({
+#             'title': code_block.title,
+#             'description': code_block.descriptions,
+#             'task': code_block.task 
+#         })
+#     else:
+#         return jsonify({"error": "Code block not found"}), 404
 
 
 ######################## fill db ########################
-with app.app_context(): 
-    db.create_all() # create the corresponding tables in your database.
-    if CodeBlock.query.count() == 0:
-        try:
-            for i in range(len(TEMPLATES)):
-                new_code_block = CodeBlock(title=TITLES[i], 
-                                            template=TEMPLATES[i], 
-                                            solution=SOLUTIONS[i], 
-                                            descriptions=DESCRIPTIONS[i],
-                                            task=TASKS[i])
-                db.session.add(new_code_block)
-            db.session.commit()
-        except Exception as e:
-            print(f"Error populating database: {e}")
-            db.session.rollback()  # Roll back changes in case of an error
-def start_server():
-    socketio_handler.run(app) 
+# with app.app_context(): 
+#     db.create_all() # create the corresponding tables in your database.
+#     if CodeBlock.query.count() == 0:
+#         try:
+#             for i in range(len(TEMPLATES)):
+#                 new_code_block = CodeBlock(title=TITLES[i], 
+#                                             template=TEMPLATES[i], 
+#                                             solution=SOLUTIONS[i], 
+#                                             descriptions=DESCRIPTIONS[i],
+#                                             task=TASKS[i])
+#                 db.session.add(new_code_block)
+#             db.session.commit()
+#         except Exception as e:
+#             print(f"Error populating database: {e}")
+#             db.session.rollback()  # Roll back changes in case of an error
 
 
 if __name__ == '__main__':
